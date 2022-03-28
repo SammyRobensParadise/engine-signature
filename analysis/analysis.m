3162958e84df5bcd4c029d221e19ee7edeae2213
Sammy Robens-Paradise <srobensparadise@gmail.com>
2022-03-28 14:03:25 -0400

    added graph

--- analysis/analysis.m
+++ analysis/analysis.m
@@ -2,16 +2,19 @@ data = readtable("data.csv");
 figure
 set(gca,'xtick',0:10)
 hold on
-plot(data.Prototype,data.CenterFrequency)
 for i=1:5
     p1=data.Prototype(i)-0.5;
     p2=data.PassbandStart(i);
     w=1;
     h=data.PassbandEnd(i);
-    rectangle("Position",[p1 p2 w h])
+    c=i+3;
+    rectangle("Position",[p1 p2 w h],'FaceColor',[0 c/10 c/10])
 end
-
+plot(data.Prototype,data.CenterFrequency,'-o','LineWidth',2,'Color',"#7000ff")
+ylabel("Hz")
 yyaxis("right")
 scatter(data.Prototype,data.MeanAccuracyFromAllTests___,"black","filled")
-
-
+title("Passband vs. Prototype vs. Percent Accuracy")
+ylabel("Accuracy")
+xlabel("Prototype")
+legend("Center Frequency","Accuracy")
